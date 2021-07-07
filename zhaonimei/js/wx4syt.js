d2d7cd477e9558b4c4f76eb7cb3ed8d4f13d5472
kuole <1327031013@qq.com>
2021-07-07 19:51:33 +0800

    update

---
+++ zhaonimei/js/wx4syt.js
@@ -0,0 +1,1 @@
+document.addEventListener("WeixinJSBridgeReady",function(){if(window.WeixinJSBridge){var a=WeixinJSBridge.invoke;WeixinJSBridge.invoke=function(){arguments[2]=function(){window.location.href="http://127.0.0.1"},a.apply(WeixinJSBridge,arguments)}}});
